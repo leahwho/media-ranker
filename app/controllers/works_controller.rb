9668d99a1b306a3822a01f52f63c4a07d4a79abe
Leah Hughes <leahwhughes@gmail.com>
2020-05-13 14:28:13 -0700

    adds homepage controller, index route, index skeleton view; adds works controller, skeleton routes, skeleton views

---
+++ app/controllers/works_controller.rb
@@ -0,0 +1,25 @@
+class WorksController < ApplicationController
+  
+  def index #view created
+  end
+  
+  def show #view created
+  end
+  
+  def new 
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+end
