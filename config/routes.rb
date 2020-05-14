aca14f17740faf8a0473eecd9dd415e6debac00a
Leah Hughes <leahwhughes@gmail.com>
2020-05-14 16:59:47 -0700

    adds user controller files, adds skeleton user index view

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   
   resources :works
   
+  resources :users, only: [:index, :show, :new, :create]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
