6e514d8c7199e4d6f733db482561b51e65db8e1e
Leah Hughes <leahwhughes@gmail.com>
2020-05-16 12:14:19 -0700

    adds validations to votes, adds vote display to work#show view

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -6,6 +6,11 @@ class VotesController < ApplicationController
     user = User.find_by(id: session[:user_id])
     work = Work.find_by(id: params[:work_id])
     
+    # TODO: need to also add if a user has already voted for the work (e.g. that vote for that work already exists in the database), flash them a message that they have already voted and can't vote again.
+    
+    # maybe create a helper method to check if the vote is in the database already?
+    
+    
     # if the user is logged in
     if session[:user_id]
       #create a new vote for those objects
@@ -22,19 +27,19 @@ class VotesController < ApplicationController
         redirect_to request.referrer || work_path(work.id)
         return
       end
-    # if there is no user logged in
+      # if there is no user logged in
     elsif !session[:user_id]
       # tell them they need to log in and redirect to who sent them there
       flash[:error] = 'A problem occurred. You must be logged in to vote.'
       redirect_to request.referrer || work_path(work.id)
       return
-    # if the user or work cannot be found
+      # if the user or work cannot be found
     elsif user.nil? || work.nil?
       head :not_found
       return
     end
   end
-    
+  
   private
   
   def votes_params
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,9 @@
 class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :work
+  
+  validates :user_id, presence: true
+  validates :work_id, presence: true
+  
+
 end
