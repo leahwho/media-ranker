5bb48898171e651add70e9680a4fbbf256c76078
Leah Hughes <leahwhughes@gmail.com>
2020-05-21 15:18:07 -0700

    adds edge case testing in work model

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -60,36 +60,38 @@ describe Work do
       expect(@work.valid?).must_equal false
       expect(@work.errors.messages).must_include :publication_year
     end
-
+    
   end 
+  
 
   describe 'relationships' do
     it 'a work can have many votes' do
       work = works(:overstory)
-
+      
       vote1 = Vote.create(work_id: work.id, user_id: users(:katie).id)
       vote2 = Vote.create(work_id: work.id, user_id: users(:leah).id)
       vote3 = Vote.create(work_id: work.id, user_id: users(:jared).id)
-
+      
       [vote1, vote2, vote3].each do |vote|
         expect(vote.valid?).must_equal true
       end
-
+      
       expect(work.votes.count).must_equal 3      
     end
-
+    
     it 'can access user through votes' do
       user1 = users(:katie)
       user2 = users(:leah)
       work = works(:blackstar)
       Vote.create(work_id: work.id, user_id: user1.id)
       Vote.create(work_id: work.id, user_id: user2.id)
-
+      
       expect(work.votes[0].user.username).must_equal user1.username
       expect(work.votes[1].user.username).must_equal user2.username
     end
   end
   
+
   describe 'custom methods' do
     
     describe 'spotlight' do
@@ -101,15 +103,28 @@ describe Work do
       
       it 'returns the correct work' do
         new_vote = Vote.create!(user_id: users(:katie).id, work_id: works(:moonstruck).id)
-
+        
         expect(Work.media_spotlight.title).must_equal 'Moonstruck'
-
+        
         new_vote2 = Vote.create!(user_id: users(:jared).id, work_id: works(:blackstar).id)
         new_vote3 = Vote.create!(user_id: users(:leah).id, work_id: works(:blackstar).id)
         expect(Work.media_spotlight.title).must_equal 'Blackstar'
-      end     
-            
+      end
+      
+      it 'returns nil if there are no works' do
+        works = [works(:brazil), works(:moonstruck), works(:overstory), works(:oryx), works(:blackstar), works(:rising)]
+        
+        works.each do |work|
+          work.destroy
+        end
+        
+        spotlight = Work.media_spotlight
+        
+        expect(spotlight).must_be_nil
+      end
+      
     end
+    
     
     describe 'categories' do
       it "puts work into correct category" do
@@ -123,33 +138,62 @@ describe Work do
         Vote.create(work_id: works(:moonstruck), user_id: users(:jared))
         Vote.create(work_id: works(:brazil), user_id: users(:katie))
         Vote.create(work_id: works(:moonstruck), user_id: users(:leah))
-
+        
         expect(Work.movies[0].title).must_equal 'Moonstruck'
         expect(Work.movies[1].title).must_equal 'Brazil'
       end
-
+      
       it "returns books sorted by high to low vote count" do
         Vote.create(work_id: works(:oryx), user_id: users(:katie))
         Vote.create(work_id: works(:oryx), user_id: users(:jared))
         Vote.create(work_id: works(:overstory), user_id: users(:katie))
         Vote.create(work_id: works(:oryx), user_id: users(:leah))
-
+        
         expect(Work.books[0].title).must_equal 'Oryx & Crake'
         expect(Work.books[1].title).must_equal 'The Overstory'
       end
-
+      
       it "returns albums sorted by high to low vote count" do
         Vote.create(work_id: works(:rising), user_id: users(:katie))
         Vote.create(work_id: works(:rising), user_id: users(:jared))
         Vote.create(work_id: works(:blackstar), user_id: users(:katie))
         Vote.create(work_id: works(:rising), user_id: users(:leah))
-
+        
         expect(Work.albums[0].title).must_equal 'Titanic Rising'
         expect(Work.albums[1].title).must_equal 'Blackstar'
       end
-
+      
+      it 'returns an empty array if there are no movies in the database' do
+        works = [works(:brazil), works(:moonstruck)]
+        
+        works.each do |work|
+          work.destroy
+        end
+        
+        expect(Work.movies).must_be_empty
+      end
+      
+      it 'returns an empty array if there are no albums in the database' do
+        works = [works(:blackstar), works(:rising)]
+        
+        works.each do |work|
+          work.destroy
+        end
+        
+        expect(Work.albums).must_be_empty
+      end
+      
+      it 'returns an empty array if there are no books in the database' do
+        works = [works(:overstory), works(:oryx)]
+        
+        works.each do |work|
+          work.destroy
+        end
+        
+        expect(Work.books).must_be_empty
+      end
+      
     end
     
   end
 end
-  
